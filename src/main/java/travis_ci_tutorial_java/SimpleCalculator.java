f423c2de817065fbdc5bc02f19b12572b5d77837
Kehinde291c <145071633+Kehinde291c@users.noreply.github.com>
2023-12-13 23:35:37 -0500

    Update SimpleCalculator.java

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,4 +4,19 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+public int minus(int a, int b) {
+    return a - b;
+}
+
+public int multiply(int a, int b) {
+    return a * b;
+}
+
+
+public int divide(int a, int b) {
+    if (b == 0) {
+        throw new IllegalArgumentException("Divider cannot be zero");
+    }
+    return a / b;
+}
 }
